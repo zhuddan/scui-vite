37f7d183f9fdc126161a78dc811e851aa849db03
zhuddan <zdecode@outlook.com>
2025-12-16 08:52:12 +0800

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   vue: true,
+  rules: {
+    'eqeqeq': 'warn',
+    'no-console': 'off',
+    'no-alert': 'off',
+  },
 })
