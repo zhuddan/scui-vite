fc7d8e2e60ab44cb3c083a7077e7e3d5e2c2309d
zhuddan <zdecode@outlook.com>
2025-12-16 08:53:21 +0800

    1

--- src/scui.js
+++ src/scui.js
@@ -72,11 +72,11 @@ export default {
     app.directive('copy', copy)
 
     // 统一注册el-icon图标
-    for (let icon in elIcons) {
+    for (const icon in elIcons) {
       app.component(`ElIcon${icon}`, elIcons[icon])
     }
     // 统一注册sc-icon图标
-    for (let icon in scIcons) {
+    for (const icon in scIcons) {
       app.component(`ScIcon${icon}`, scIcons[icon])
     }
 
